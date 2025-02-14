3579e189e1610374ade755bbb37e600c7506728c
Conor153 <157692610+Conor153@users.noreply.github.com>
2025-02-14 10:38:07 +0000

    Created file structure with node package manager installs

---
+++ Film.js
@@ -0,0 +1,21 @@
+class Film
+{
+    constructor(name, category, genre, director, coverImage, VideoURL, ReleaseDate)
+    {
+        this.name = name;
+        this.category = category;
+        this.genre = genre,
+        
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
