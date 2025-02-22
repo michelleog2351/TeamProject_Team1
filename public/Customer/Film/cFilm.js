849919d5bb911fee9f9bac28d45b2f7a1bc92946
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-02-22 21:31:22 +0000

    populate dropdown menus and add cover images to home page

--- public/Customer/Film/cFilm.js
+++ public/Customer/Film/cFilm.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    if (typeof nav === 'function') nav();
-    if (typeof footer === 'function') footer();
-    fetchFilms();
+  if (typeof nav === "function") nav();
+  if (typeof footer === "function") footer();
+  fetchFilms();
 });
 
 function fetchFilms() {
-    $('#filmCards').empty();
-    $.getJSON(`http://localhost:3000/films`, function (data) {
-        $.each(data, function (i, value) {
-            let releaseDate = new Date(value.ReleaseDate).toISOString().split('T')[0];
-            $('#filmCards').append(`
+  $("#filmCards").empty();
+  $.getJSON(`http://localhost:3000/films`, function (data) {
+    $.each(data, function (i, value) {
+      let releaseDate = new Date(value.ReleaseDate).toISOString().split("T")[0];
+      $("#filmCards").append(`
                 <div class="col-md-4 mb-3">
                     <div class="card">
                         <img src="${value.CoverImage}" class="card-img-top img-fluid" alt="${value.Name}" style="height: 200px; object-fit: cover;">
@@ -24,13 +24,16 @@ function fetchFilms() {
                     </div>
                 </div>
             `);
-        });
-        $(document).on("click", ".viewDetailsButton", function () {
-            let filmID = $(this).data("id");
-            console.log("Selected Film ID:", filmID);
-            location.href = `filmDetails.html?filmID=${filmID}`;
-        });
-    }).fail(function () {
-        alert("Failed to load films.");
     });
+
+    // <img src="../../images/${value.Name.replace(/\s+/g, "_")}.jpg" can replace line 15 with this if you want to view the images
+
+    $(document).on("click", ".viewDetailsButton", function () {
+      let filmID = $(this).data("id");
+      console.log("Selected Film ID:", filmID);
+      location.href = `filmDetails.html?filmID=${filmID}`;
+    });
+  }).fail(function () {
+    alert("Failed to load films.");
+  });
 }
