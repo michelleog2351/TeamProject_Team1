8c43d38f986d4582ebeef4cd0359021edb0b54ba
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-02-22 13:14:01 +0000

    Set navbar to centre and add table-hover

--- public/app.js
+++ public/app.js
@@ -2,7 +2,49 @@ $(document).ready(function () {
   nav();
   footer();
 
-  search();
+  //search();
+  
 });
 
-function search() {}
+// function search() {
+//   $.getJSON(http://localhost:3000/films, function (data) {
+//     $.each(data, function (i, value) {
+//       let releaseDate = new Date(value.ReleaseDate).toISOString().split("T")[0];
+//       $("#filmCards").append(
+//                 <div class="col-md-4 mb-3">
+//                     <div class="card">
+//                         <img src="${value.CoverImage}" class="card-img-top img-fluid" alt="${value.Name}" style="height: 200px; object-fit: cover;">
+//                         <div class="card-body">
+//                             <h5 class="card-title">${value.Name}</h5>
+//                             <p class="card-text">${value.Category} | ${value.Genre}</p>
+//                             <p class="card-text"><small>Released: ${releaseDate}</small></p>
+//                             <button type="button" class="btn btn-primary viewDetailsButton" data-id="${value.FilmID}">
+//                                 View Details
+//                             </button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             );
+//     });
+//     $(document).on("click", ".viewDetailsButton", function () {
+//       let filmID = $(this).data("id");
+//       console.log("Selected Film ID:", filmID);
+//       location.href = filmDetails.html?filmID=${filmID};
+//     });
+//   }).fail(function () {
+//     alert("Failed to load films.");
+//   });
+
+//   var filteredProductsCount = await productsQuery.CountAsync();
+// if (!string.IsNullOrEmpty(searchString))
+// {
+//     if (filteredProductsCount == 0)
+//     {
+//         ViewBag.SearchMsg = $"No results found for '{searchString}'.";
+//     }
+//     else
+//     {
+//         ViewBag.SearchMsg = $"Showing {filteredProductsCount} result(s) for '{searchString}'.";
+//     }
+// }
+// }
--- public/nav.js
+++ public/nav.js
@@ -1,31 +1,31 @@
 function nav() {
   var navOutPut = `
-    <ul class="navbar-nav" style="padding: 15px 15px 15px 15px">
-<li class="nav-item">
-            <a class="nav-link" href="/index.html">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/film.html">Film</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/ticket.html">Ticket</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/screening/screening.html">Screening</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/theatre.html">Theatre</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/admin.html">Admin</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/manager.html">Manager</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="/Customer/Film/cFilm.html">Films Showing</a>
-        </li>
-    
+    <div class="container d-flex justify-content-center"> 
+        <ul class="navbar-nav" style="padding: 15px 15px 15px 15px">
+            <li class="nav-item">
+                <a class="nav-link" href="/index.html">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/film.html">Film</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/ticket.html">Ticket</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/screening/screening.html">Screening</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/theatre.html">Theatre</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin.html">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/manager.html">Manager</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/Customer/Film/cFilm.html">Films Showing</a>
+            </li>    
     `;
   if (sessionStorage.getItem("login") == "true") {
     navOutPut += `
@@ -40,6 +40,6 @@ function nav() {
         <a class="nav-link" href="/login.html">Login</a> 
         </li>`;
   }
-  navOutPut += `</ul>`;
+  navOutPut += `</ul></div>`;
   $("nav").html(navOutPut);
 }
